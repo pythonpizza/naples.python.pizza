7393aa7536d41ccc85f2d4ace4e2cb3e243a0103
Patrick Arminio <patrick.arminio@gmail.com>
2017-08-04 22:37:31 +0100

    Add actions

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,6 +3,7 @@ import style from './style';
 
 import Logo from '../../components/logo';
 import MapBackground from '../../components/map-background';
+import { ButtonLink } from '../../components/button';
 
 export default class Home extends Component {
 	render() {
@@ -15,6 +16,21 @@ export default class Home extends Component {
 					<h1>python.pizza</h1>
 					<p>Riot Studio, Napoli</p>
 					<p>16 Settembre 2017</p>
+				</div>
+
+				<div class={style.actions}>
+					<ButtonLink
+						target="_blank"
+						href="https://www.eventbrite.it/e/pythonpizza-tickets-36788948736"
+					>
+						Registrati
+					</ButtonLink>
+					<ButtonLink
+						target="_blank"
+						href="https://drive.google.com/file/d/0BzLKfHQcu_LwZWs0YlRDRWZXcW8/view?usp=sharing"
+					>
+						Sponsorizza
+					</ButtonLink>
 				</div>
 			</div>
 		);
