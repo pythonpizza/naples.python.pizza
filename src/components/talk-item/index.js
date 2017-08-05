3d51e4a8fd72497c37a42dfbf2dc4f18f796644a
Patrick Arminio <patrick.arminio@gmail.com>
2017-08-05 14:39:48 +0100

    Add initial styles for the talk list

--- src/components/talk-item/index.js
+++ src/components/talk-item/index.js
@@ -4,9 +4,24 @@ import style from './style';
 
 const TalkItem = () =>
 	<li class={style.item}>
-		<div>TIME</div>
-		<div>TITLE</div>
-		<div>Description</div>
+		<div>
+			<div class={style.time}>18.30</div>
+			<div class={style.header}>
+				<div class={style.avatar}>
+					<img src="https://pbs.twimg.com/profile_images/3760396480/667e792df246fa1d455c76faff615f30_400x400.jpeg" />
+				</div>
+				<div class={style.title}>
+					Tensorflow per OCR: da zero allo stato dell'arte
+				</div>
+			</div>
+		</div>
+		<div class={style.description}>
+			Proin ac volutpat tortor. Integer at tortor ex. Aenean pretium quis ipsum
+			vel pellentesque. Sed ullamcorper, libero ut auctor varius, lectus quam
+			semper felis, vel consectetur leo odio id ante. In sit amet augue eu lorem
+			mollis maximus. Pellentesque et risus orci. Lorem ipsum dolor sit amet,
+			consectetur adipiscing elit. Vivamus id placerat elit.
+		</div>
 	</li>;
 
 export default TalkItem;
