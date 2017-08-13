3368a50c274c01a469cf5ee29a089df8272c883b
Patrick Arminio <patrick.arminio@gmail.com>
2017-08-13 18:16:17 +0100

    Pull talk information out

--- src/components/talk-item/index.js
+++ src/components/talk-item/index.js
@@ -2,9 +2,8 @@ import { h } from 'preact';
 
 import style from './style';
 
-const TalkItem = () => (
+const TalkItem = ({ talk }) =>
 	<li class={style.item}>
-		<div class={style.time}>18.30</div>
 		<div>
 			<div>
 				<div class={style.header}>
@@ -14,27 +13,24 @@ const TalkItem = () => (
 								clip-path="url(#avatar-mask)"
 								height="100%"
 								width="100%"
-								xlinkHref="https://pbs.twimg.com/profile_images/3760396480/667e792df246fa1d455c76faff615f30_400x400.jpeg"
+								xlinkHref="talk.author.avatar"
 							/>
 						</svg>
 					</div>
 					<div>
 						<div class={style.title}>
-							Tensorflow per OCR: da zero allo stato dell'arte
+							{talk.title}
 						</div>
 					</div>
 				</div>
 			</div>
 			<div class={style.description}>
-				Proin ac volutpat tortor. Integer at tortor ex. Aenean pretium quis
-				ipsum vel pellentesque. Sed ullamcorper, libero ut auctor varius, lectus
-				quam semper felis, vel consectetur leo odio id ante. In sit amet augue
-				eu lorem mollis maximus. Pellentesque et risus orci. Lorem ipsum dolor
-				sit amet, consectetur adipiscing elit. Vivamus id placerat elit.
-				<div class={style.author}>— Vincenzo Santopietro</div>
+				{talk.description}
+				<div class={style.author}>
+					— {talk.author.name}
+				</div>
 			</div>
 		</div>
-	</li>
-);
+	</li>;
 
 export default TalkItem;
--- src/components/talk-list/index.js
+++ src/components/talk-list/index.js
@@ -3,11 +3,25 @@ import { h } from 'preact';
 import TalkItem from '../talk-item';
 import style from './style';
 
-const talks = [1, 2, 3];
+const talks = [
+	{
+		title: "Tensorflow per OCR: da zero allo stato dell'arte",
+		description: `Proin ac volutpat tortor. Integer at tortor ex. Aenean pretium quis
+		ipsum vel pellentesque. Sed ullamcorper, libero ut auctor varius, lectus
+		quam semper felis, vel consectetur leo odio id ante. In sit amet augue
+		eu lorem mollis maximus. Pellentesque et risus orci. Lorem ipsum dolor
+		sit amet, consectetur adipiscing elit. Vivamus id placerat elit.`,
+		author: {
+			name: 'Vincenzo Santopietro',
+			avatar:
+				'https://pbs.twimg.com/profile_images/3760396480/667e792df246fa1d455c76faff615f30_400x400.jpeg'
+		}
+	}
+];
 
 const TalkList = ({ children }) =>
 	<ol class={style.list}>
-		{talks.map(index => <TalkItem key={index} />)}
+		{talks.map((talk, index) => <TalkItem key={index} talk={talk} />)}
 	</ol>;
 
 export default TalkList;
