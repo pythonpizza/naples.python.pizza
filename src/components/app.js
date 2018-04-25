3d0dba6f97b5aeb406443b087e6234a0aeb1ea1d
Marco Acierno <marcoaciernoemail@gmail.com>
2018-04-25 20:42:13 +0200

    Revert "Add path /2017 for the 2017 python.pizza website"
    This reverts commit 17c9ef7a71802e99aa7a4cc3261e626fc4ce85e9.

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,10 @@
 import { h } from 'preact';
-import Router from 'preact-router';
 
 import Home from '../routes/home';
-import Home2017 from '../routes/home-2017';
 
 const App = () =>
 	<div id="app">
-		<Router>
-			<Home path="/" />
-			<Home2017 path="/2017" />
-		</Router>
+		<Home />
 	</div>;
 
 export default App;
