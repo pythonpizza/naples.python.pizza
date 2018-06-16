417de8c78d1fd23a8e17d0497fb482ac51123fcc
Marco Acierno <marcoaciernoemail@gmail.com>
2018-06-16 22:44:41 +0200

    Mobile fixes

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,8 @@ module.exports = {
         'postcss-nested': {},
         'postcss-custom-media': {
             extensions: {
-                '--mobile': '(min-width: 375px)',
+                // '--mobile': '(min-width: 375px)',
+                '--mobile': '(min-width: 480px)',
                 '--tablet': '(min-width: 769px)',
             },
             appendExtensions: true,
