6a4d4d0d71ec751b0e7aeefc4a4d6b3386745155
Dario Carella <dario.kar@gmail.com>
2018-07-08 12:33:44 +0200

    Fix speakers grid layout

--- postcss.config.js
+++ postcss.config.js
@@ -3,9 +3,9 @@ module.exports = {
         'postcss-nested': {},
         'postcss-custom-media': {
             extensions: {
-                // '--mobile': '(min-width: 375px)',
                 '--mobile': '(min-width: 480px)',
                 '--tablet': '(min-width: 769px)',
+                '--laptop': '(min-width: 1025px)',
             },
             appendExtensions: true,
         },
