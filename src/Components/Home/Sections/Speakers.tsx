871947f1dd3ed72665da8340dc59eaed5c15f49c
Marco Acierno <marcoaciernoemail@gmail.com>
2018-07-09 19:50:50 +0200

    Add speakers list

--- src/Components/Home/Sections/Speakers.tsx
+++ src/Components/Home/Sections/Speakers.tsx
@@ -3,20 +3,10 @@ import * as React from 'react';
 import Container, { Sizes } from '@/Components/Container';
 import Grid from '@/Components/Grid';
 import SpeakerCard from '@/Components/SpeakerCard';
-import Speaker from '@/Types/Speaker';
+import { SPEAKERS } from '@/dataset';
 
 export default class Speakers extends React.Component {
     render() {
-        let speakers: Speaker[] = [
-            {
-                name: 'Patrick Arminio',
-                photo:
-                    'https://images.pexels.com/photos/415263/pexels-photo-415263.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
-                talk: 'GraphQL & Frontend',
-            },
-        ];
-        speakers = new Array(9).fill(speakers[0], 0, 9);
-
         return (
             <section id="speakers" className="speakers">
                 <Container size={Sizes.large}>
@@ -27,7 +17,7 @@ export default class Speakers extends React.Component {
                             Yup babel debugger lodash s3 classnames webpack. Stackoverflow cloud throttle grunt reactive
                         </p>
                     </Container>
-                    <Grid>{speakers.map((speaker, i) => <SpeakerCard key={i} speaker={speaker} />)}</Grid>
+                    <Grid>{SPEAKERS.map((speaker, i) => <SpeakerCard key={i} speaker={speaker} />)}</Grid>
                 </Container>
             </section>
         );
---
+++ src/dataset.ts
@@ -0,0 +1,29 @@
+import Speaker from '@/Types/Speaker';
+
+export const SPEAKERS: Speaker[] = [
+    {
+        name: 'Patrick Guido Arminio',
+        photo: 'https://pbs.twimg.com/profile_images/919207390880456705/gqcS3mMB_400x400.jpg',
+        talk: 'TBA',
+    },
+    {
+        name: 'Fiorella De Luca',
+        photo: 'https://pbs.twimg.com/profile_images/914168800962121729/8_kIl1M-_400x400.jpg',
+        talk: 'TBA',
+    },
+    {
+        name: 'Andrea Stagi',
+        photo: 'https://pbs.twimg.com/profile_images/963179422475083778/wcQt7MlF_400x400.jpg',
+        talk: 'TBA',
+    },
+    {
+        name: 'Salvatore Busiello',
+        photo: 'https://pbs.twimg.com/profile_images/931954046822502400/8yONc3hg_400x400.jpg',
+        talk: 'TBA',
+    },
+    {
+        name: 'Vincenzo Santopietro',
+        photo: 'https://pbs.twimg.com/profile_images/928684714185064448/jhl36f7W_400x400.jpg',
+        talk: 'TBA',
+    },
+];
