7583bc9a55289ee9aa3a20b0117e81dbb36c17b9
Marco Acierno <marcoaciernoemail@gmail.com>
2018-07-18 19:42:54 +0200

    Add speakers job

--- src/dataset.ts
+++ src/dataset.ts
@@ -4,36 +4,36 @@ export const SPEAKERS: Speaker[] = [
     {
         name: 'Patrick Guido Arminio',
         photo: 'https://pbs.twimg.com/profile_images/919207390880456705/gqcS3mMB_400x400.jpg',
-        talk: 'TBA',
+        job: 'Stink Studios',
     },
     {
         name: 'Fiorella De Luca',
         photo: 'https://pbs.twimg.com/profile_images/914168800962121729/8_kIl1M-_400x400.jpg',
-        talk: 'TBA',
+        job: 'Women Techmakers',
     },
     {
         name: 'Andrea Stagi',
         photo: 'https://pbs.twimg.com/profile_images/963179422475083778/wcQt7MlF_400x400.jpg',
-        talk: 'TBA',
+        job: 'Lotrek',
     },
     {
         name: 'Salvatore Busiello',
         photo: 'https://pbs.twimg.com/profile_images/931954046822502400/8yONc3hg_400x400.jpg',
-        talk: 'TBA',
+        job: 'Lotrek',
     },
     {
         name: 'Vincenzo Santopietro',
         photo: 'https://pbs.twimg.com/profile_images/928684714185064448/jhl36f7W_400x400.jpg',
-        talk: 'TBA',
+        job: 'Intel Student Ambassador',
     },
     {
         name: 'Iacopo Spalletti',
         photo: 'https://pbs.twimg.com/profile_images/2527288391/wr949gu8t2n995u19h98_400x400.png',
-        talk: 'TBA',
+        job: 'Nephila',
     },
     {
         name: 'Valerio Maggio',
         photo: 'https://pbs.twimg.com/profile_images/955716617006211072/vW5eTirr_400x400.jpg',
-        talk: 'TBA',
+        job: 'Fondazione Bruno Kessler',
     },
 ];
