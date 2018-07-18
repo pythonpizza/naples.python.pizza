5260a246bae6f30e45d86f2340e356ae8950a45e
Marco Acierno <marcoaciernoemail@gmail.com>
2018-07-18 20:10:08 +0200

    Add smooth scroll

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 
+import * as smoothscroll from 'smoothscroll-polyfill';
+
 import Home from '@/Components/Home';
 import Header from '@/Components/Header';
 
 import './reset.css';
 import './theme.css';
 import './typography.css';
+
+smoothscroll.polyfill();
 
 export default class App extends React.Component {
     render() {
