cf6b2fb65baeec4304f4d07bb5199a8553c3dfa6
Marco Acierno <marcoaciernoemail@gmail.com>
2018-08-01 10:54:32 +0200

    Add rel="noopener noreferrer" to external links

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -11,7 +11,7 @@ export default class Hero extends React.Component {
                 <div className="hero--info">
                     <h1>Python pizza</h1>
                     <h2>
-                        <a target="_blank" href="https://www.google.com/maps/place/Palazzo+delle+Arti+Napoli/@40.8368426,14.2346782,17z/data=!3m1!4b1!4m5!3m4!1s0x133b08ffaf19f077:0x29aeaac14bce93b2!8m2!3d40.8368426!4d14.2368669">
+                        <a href="https://www.google.com/maps/place/Palazzo+delle+Arti+Napoli/@40.8368426,14.2346782,17z/data=!3m1!4b1!4m5!3m4!1s0x133b08ffaf19f077:0x29aeaac14bce93b2!8m2!3d40.8368426!4d14.2368669" target="_blank" rel="noopener noreferrer">
                             Napoli @ PAN
                         </a>
                     </h2>
