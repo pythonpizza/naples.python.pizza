b49f6ec47f1b7cc35fe290f549380c5a3445d346
Marco Acierno <marcoaciernoemail@gmail.com>
2019-05-09 09:37:40 +0200

    Initial updates for the 2019 edition

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -11,11 +11,11 @@ export default class Hero extends React.Component {
                 <div className="hero--info">
                     <h1>Python Pizza</h1>
                     <h2>
-                        <a href="https://www.google.com/maps/place/Palazzo+delle+Arti+Napoli/@40.8368426,14.2346782,17z/data=!3m1!4b1!4m5!3m4!1s0x133b08ffaf19f077:0x29aeaac14bce93b2!8m2!3d40.8368426!4d14.2368669" target="_blank" rel="noopener noreferrer">
-                            Napoli @ PAN
+                        <a href="#" target="_blank" rel="noopener noreferrer">
+                            Napoli
                         </a>
                     </h2>
-                    <h2>15 Settembre 2018</h2>
+                    <h2>14 Settembre 2019</h2>
                 </div>
 
                 <Wave />
